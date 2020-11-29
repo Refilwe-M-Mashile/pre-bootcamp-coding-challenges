569fea61d6133c80acebdb0376ce60ebd26827d2
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2020-11-29 10:29:21 +0200

    Updated variable name of semi-perimeter on task 5

--- task5.js
+++ task5.js
@@ -1,6 +1,6 @@
 function calcArea(a,b,c){
-   var semiP = 0.5*(a+b+c); //semiPerimeter
-   return Math.sqrt(semiP*(semiP-a)*(semiP-b)*(semiP-c)); //Heron's formula
+   var semiPerimeter = 0.5*(a+b+c); 
+   return Math.sqrt(semiPerimeter*(semiPerimeter-a)*(semiPerimeter-b)*(semiPerimeter-c)); //Heron's formula
 }
 
-console.log("Area: " + calcArea(5,4,3)); //6
+console.log("Area: " + calcArea(5,4,3));
