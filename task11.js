3dbb468db958b93f996d2e909d3f003c8b028abd
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2020-11-29 10:51:09 +0200

    Removed comments and updated function name (getVowels -> getCommons)

--- task11.js
+++ task11.js
@@ -1,11 +1,11 @@
-function getVowels(string1, string2){
-    var commons = [];
+function getCommons(string1, string2){
     
-    //case sensitive
+    var commons = [];
+
     for(var i = 0; i< string1.length; i++){
         for(var j = 0; j < string2.length; j++){
             if(string1[i] === string2[j]){
-                if(!commons.includes(string1[i])){ // check if letter is already in commons
+                if(!commons.includes(string1[i])){
                     commons.push(string1[i]);
                 }
             }
@@ -15,4 +15,4 @@ function getVowels(string1, string2){
     console.log("Common Letters: " + commons);
 }
 
-getVowels("house", "computers");
+getCommons("house", "computers");
