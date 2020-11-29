2f3584dee12c7be3f6aaecdec4071126111af34c
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2020-11-29 10:25:22 +0200

    Removed comments on this task

--- task4.js
+++ task4.js
@@ -1,11 +1,8 @@
 function hasThree(num1, num2){
-    var sum = "" + (num1 + num2); // convert to string
+    var sum = "" + (num1 + num2); // converts to string
     if((num1 === 3 || num2 === 3) && (sum.includes("3"))){
         return true;
     }else{
         return false;
     }
 }
-
-var isIt = hasThree(3,27);
-console.log(isIt);
