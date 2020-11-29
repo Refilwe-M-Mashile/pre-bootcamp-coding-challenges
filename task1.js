6adc3243e8cdd99a0bc7b94175ed551039ef5ca5
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2020-11-29 10:10:51 +0200

    removed comments on task 1

--- task1.js
+++ task1.js
@@ -1,11 +1,11 @@
 var x = 0;
 var y = 1;
 
-console.log("X:" + x); //0
-console.log("Y:" + y); //1
+console.log("X:" + x);
+console.log("Y:" + y);
 
 x += 3;
 y += x;
 
-console.log("X:" + x); //3
-console.log("Y:" + y); //4
+console.log("X:" + x);
+console.log("Y:" + y);
