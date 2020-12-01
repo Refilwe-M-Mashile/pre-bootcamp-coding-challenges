a84345e666d56b09df6341edfec2aa97f0ac10c0
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2020-12-01 20:27:18 +0200

    formatted function

--- task8.js
+++ task8.js
@@ -1,22 +1,22 @@
-function getTime(num){
-    var hours = Math.floor(num/60);
-    var mins = num % 60; 
-    var output = "";
-    
-    //handling plurals and singulars
-    if(hours>1){
-        output += hours + " hours, ";   
-    }else{
-        output += hours + " hour, ";   
-    }
+function getTime(num) {
+  var hours = Math.floor(num / 60);
+  var mins = num % 60;
+  var output = "";
 
-    if(mins>1){
-        output += mins + " minutes";
-    }else{
-        output += mins + " minute";
-    }
+  //handling plurals and singulars
+  if (hours > 1) {
+    output += hours + " hours, ";
+  } else {
+    output += hours + " hour, ";
+  }
 
-    console.log(output);
+  if (mins > 1) {
+    output += mins + " minutes";
+  } else {
+    output += mins + " minute";
+  }
+
+  console.log(output);
 }
 
 getTime(121);
