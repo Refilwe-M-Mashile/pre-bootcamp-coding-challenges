f4e84caf70c0a0fefa5562f57ca09b6f2167a6ba
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2020-12-01 20:31:41 +0200

    Task 9-11 functions formatted with the prettier tool

--- task10.js
+++ task10.js
@@ -1,42 +1,38 @@
-function getVowels(word){
-   var vowels = [];
-    for(var i = 0; i < word.length; i++){
-        var char = word[i];
-        switch(char){
-            case "A": //uppercase
-            case "a": //lowercase
-                if(!vowels.includes("a")){
-                    vowels.push("a");
-                }
-                break;
-            case "E":
-            case "e":
-                if(!vowels.includes("e")){
-                    vowels.push("e");
-                }
-                break;
-            case "I":
-            case "i":
-                if(!vowels.includes("i")){
-                    vowels.push("i");
-                }
-                break;
-            case "O":
-            case "o":
-                if(!vowels.includes("o"))
-                vowels.push("o");
-                break;
-            case "U":
-            case "u":
-                if(!vowels.includes("u"))
-                vowels.push("u");
-                break;
-
+function getVowels(word) {
+  var vowels = [];
+  for (var i = 0; i < word.length; i++) {
+    var char = word[i];
+    switch (char) {
+      case "A": //uppercase
+      case "a": //lowercase
+        if (!vowels.includes("a")) {
+          vowels.push("a");
         }
+        break;
+      case "E":
+      case "e":
+        if (!vowels.includes("e")) {
+          vowels.push("e");
+        }
+        break;
+      case "I":
+      case "i":
+        if (!vowels.includes("i")) {
+          vowels.push("i");
+        }
+        break;
+      case "O":
+      case "o":
+        if (!vowels.includes("o")) vowels.push("o");
+        break;
+      case "U":
+      case "u":
+        if (!vowels.includes("u")) vowels.push("u");
+        break;
     }
+  }
 
-    console.log(vowels);
-
+  console.log(vowels);
 }
 
 getVowels("RefIlwe");
--- task11.js
+++ task11.js
@@ -1,18 +1,17 @@
-function getCommons(string1, string2){
-    
-    var commons = [];
+function getCommons(string1, string2) {
+  var commons = [];
 
-    for(var i = 0; i< string1.length; i++){
-        for(var j = 0; j < string2.length; j++){
-            if(string1[i] === string2[j]){
-                if(!commons.includes(string1[i])){
-                    commons.push(string1[i]);
-                }
-            }
+  for (var i = 0; i < string1.length; i++) {
+    for (var j = 0; j < string2.length; j++) {
+      if (string1[i] === string2[j]) {
+        if (!commons.includes(string1[i])) {
+          commons.push(string1[i]);
         }
+      }
     }
+  }
 
-    console.log("Common Letters: " + commons);
+  console.log("Common Letters: " + commons);
 }
 
 getCommons("house", "computers");
